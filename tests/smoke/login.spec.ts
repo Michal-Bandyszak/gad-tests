aec12221cbc111ca93e122573a1c82543eab9fcb
michal_bandyszak <michalbandyszak@gmail.com>
2024-05-14 11:14:29 +0200

    test: login scenario

---
+++ tests/smoke/login.spec.ts
@@ -0,0 +1,23 @@
+import { LoginPage } from '../../src/pages/login.page';
+import { WelcomePage } from '../../src/pages/welcome.page';
+import { expect, test } from '@playwright/test';
+
+test.describe('Verify login', () => {
+  test('User login test with correct credentials @GAD_R02_01', async ({
+    page,
+  }) => {
+    // Arrange
+    const userEmail = 'Moses.Armstrong@Feest.ca';
+    const userPassword = 'test1';
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+
+    // Act
+    await loginPage.login(userEmail, userPassword);
+    const welcomePage = new WelcomePage(page);
+    const title = await welcomePage.title();
+
+    //Assert
+    expect(title).toContain('Welcome');
+  });
+});
