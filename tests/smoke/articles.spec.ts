a983eef5fd023406c243b176e68a7609f7481bd6
michal_bandyszak <michalbandyszak@gmail.com>
2024-07-06 13:02:44 +0200

    test: Negative tests for article

--- tests/smoke/articles.spec.ts
+++ tests/smoke/articles.spec.ts
@@ -34,4 +34,55 @@ test.describe('Verify Articles', () => {
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
   });
+
+  test('Reject creating article without title field #GAD_R04_01 @S04', async ({
+    page,
+  }) => {
+    // Arrange
+    const expectedErrorText = 'Article was not created';
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(testUser1);
+
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+
+    //Act
+    await articlesPage.addArticleButtonLogged.click();
+
+    const addArticleView = new AddArticleView(page);
+    await expect.soft(addArticleView.header).toBeVisible();
+
+    const articleData = randomNewArticle();
+    articleData.title = '';
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorText);
+  });
+  test('Reject creating article without body field #GAD_R04_01 @S04', async ({
+    page,
+  }) => {
+    // Arrange
+    const expectedErrorText = 'Article was not created';
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(testUser1);
+
+    const articlesPage = new ArticlesPage(page);
+    await articlesPage.goto();
+
+    //Act
+    await articlesPage.addArticleButtonLogged.click();
+
+    const addArticleView = new AddArticleView(page);
+    await expect.soft(addArticleView.header).toBeVisible();
+
+    const articleData = randomNewArticle();
+    articleData.body = '';
+    await addArticleView.createArticle(articleData);
+
+    //Assert
+    await expect(addArticleView.alertPopup).toHaveText(expectedErrorText);
+  });
 });
