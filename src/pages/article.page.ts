6f780d99d8f41c1adfb8e016761d4f23d35b0b5d
michal_bandyszak <michalbandyszak@gmail.com>
2024-07-30 10:35:42 +0200

    chore: Article handling comments & popup

--- src/pages/article.page.ts
+++ src/pages/article.page.ts
@@ -1,11 +1,17 @@
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
+interface ArticleComment {
+  body: Locator;
+  link: Locator;
+}
 export class ArticlePage extends BasePage {
   url = '/article.html';
   articleTitle = this.page.getByTestId('article-title');
   articleBody = this.page.getByTestId('article-body');
   deleteArticleIcon = this.page.getByTestId('delete');
+  addCommentButton = this.page.locator('#add-new');
+  alertPopup = this.page.getByTestId('alert-popup');
   constructor(page: Page) {
     super(page);
   }
@@ -14,5 +20,16 @@ export class ArticlePage extends BasePage {
       await dialog.accept();
     });
     this.deleteArticleIcon.click();
+  }
+
+  getArticleComment(body: string): ArticleComment {
+    const commentContainer = this.page
+      .locator('.comment-container')
+      .filter({ hasText: body });
+
+    return {
+      body: commentContainer.locator(':text("comment: ") + span'),
+      link: commentContainer.locator("[id^='gotoComment']"),
+    };
   }
 }
