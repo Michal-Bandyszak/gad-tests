a8f9c5582b9425b698c99ed5cb910f4926a34155
michal_bandyszak <michalbandyszak@gmail.com>
2024-08-21 09:24:57 +0200

    wip: handling session

---
+++ tests/setup/login.setup.ts
@@ -0,0 +1,22 @@
+import { STORAGE_STATE } from '../../playwright.config';
+import { LoginPage } from '../../src/pages/login.page';
+import { WelcomePage } from '../../src/pages/welcome.page';
+import { testUser1 } from '../../src/test-data/user.data';
+import { expect, test as setup } from '@playwright/test';
+
+setup('Login with correct credentials @logged', async ({ page }) => {
+  // Arrange
+  const expectedWelcomeTitle = 'Welcome';
+  const loginPage = new LoginPage(page);
+  const welcomePage = new WelcomePage(page);
+
+  // Act
+  await loginPage.goto();
+  await loginPage.login(testUser1);
+
+  const title = await welcomePage.getTitle();
+
+  //Assert
+  expect(title).toContain(expectedWelcomeTitle);
+  await page.context().storageState({ path: STORAGE_STATE });
+});
--- tests/smoke/articles.spec.ts
+++ tests/smoke/articles.spec.ts
@@ -2,30 +2,25 @@ import { prepareRandomArticle } from '../../src/factories/article.factory';
 import { AddArticleModel } from '../../src/models/article.model';
 import { ArticlePage } from '../../src/pages/article.page';
 import { ArticlesPage } from '../../src/pages/articles.page';
-import { LoginPage } from '../../src/pages/login.page';
-import { testUser1 } from '../../src/test-data/user.data';
 import { AddArticleView } from '../../src/views/add-article.view';
+// import { testUser1 } from '../../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Create and verify Article', () => {
-  let loginPage: LoginPage;
   let articlesPage: ArticlesPage;
   let articlePage: ArticlePage;
   let addArticleView: AddArticleView;
   let articleData: AddArticleModel;
 
   test.beforeEach(async ({ page }) => {
-    loginPage = new LoginPage(page);
     articlesPage = new ArticlesPage(page);
     addArticleView = new AddArticleView(page);
     articlePage = new ArticlePage(page);
 
-    await loginPage.goto();
-    await loginPage.login(testUser1);
     await articlesPage.goto();
   });
 
-  test('User can create article with mandatory fields #GAD-R04-01 @S04', async () => {
+  test('User can create article with mandatory fields #GAD-R04-01 @S04 @logged', async () => {
     // Arrange
     articleData = prepareRandomArticle();
 
@@ -50,5 +45,21 @@ test.describe('Create and verify Article', () => {
     await expect
       .soft(articlePage.articleBody)
       .toHaveText(articleData.body, { useInnerText: true });
+  });
+
+  test.describe('title length', () => {
+    test('reject creating article with title exceeding 128 signs @GAD-R04-02 @logged', async () => {
+      const expectedErrorMessage = 'Article was not created';
+      const articleData = prepareRandomArticle(129);
+
+      await articlesPage.addArticleButtonLogged.click();
+      await expect.soft(addArticleView.addNewHeader).toBeVisible();
+
+      // Act
+      await addArticleView.createArticle(articleData);
+
+      //Assert
+      await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+    });
   });
 });
