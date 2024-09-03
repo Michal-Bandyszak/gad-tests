84c917786cda99279b3b5f49bf07c03d7e95dc5e
michal_bandyszak <michalbandyszak@gmail.com>
2024-09-03 08:17:10 +0200

    chore: refactor comment page return po

--- src/pages/comment.page.ts
+++ src/pages/comment.page.ts
@@ -1,13 +1,28 @@
+import { MainMenuComponent } from '@_src/components/main-menu.component';
+import { ArticlePage } from '@_src/pages/article.page';
 import { BasePage } from '@_src/pages/base.page';
+import { EditCommentView } from '@_src/views/edit-comment.view';
 import { Page } from '@playwright/test';
 
 export class CommentPage extends BasePage {
   url = '/comment.html';
+  mainMenu = new MainMenuComponent(this.page);
   commentBody = this.page.getByTestId('comment-body');
   editButton = this.page.getByTestId('edit');
   alertPopup = this.page.getByTestId('alert-popup');
   returnLink = this.page.getByTestId('return');
+
   constructor(page: Page) {
     super(page);
+  }
+
+  async clickEditButton(): Promise<EditCommentView> {
+    await this.editButton.click();
+    return new EditCommentView(this.page);
+  }
+
+  async clickReturnLink(): Promise<ArticlePage>{
+    await this.returnLink.click();
+    return new ArticlePage(this.page);
   }
 }
