4f43d103086a4637703549a229a7ce1ab434a1a8
michal_bandyszak <michalbandyszak@gmail.com>
2024-09-24 09:17:26 +0200

    chore: wip adding fixtures to webpage.spec

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,10 +1,12 @@
 import { expect, test as baseTest } from '@playwright/test';
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
+import { HomePage } from '@_src/pages/home.page';
 
 interface Pages {
   articlesPage: ArticlesPage;
   commentsPage: CommentsPage;
+  homePage: HomePage
 }
 
 export const pageObjectTest = baseTest.extend<Pages>({
@@ -17,5 +19,10 @@ export const pageObjectTest = baseTest.extend<Pages>({
     const commentsPage = new CommentsPage(page);
     await commentsPage.goto();
     await use(commentsPage);
+  },
+  homePage: async({page}, use) => {
+    const homePage = new HomePage(page);
+    await homePage.goto();
+    await use(homePage)
   }
 })
