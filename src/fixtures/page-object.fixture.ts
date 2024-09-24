4af33d9419dd97470aa5f701ffd8cd6c1698f8b8
michal_bandyszak <michalbandyszak@gmail.com>
2024-09-24 09:29:02 +0200

    chore: added own fixture to register spec

--- src/fixtures/page-object.fixture.ts
+++ src/fixtures/page-object.fixture.ts
@@ -1,35 +1,42 @@
-import { expect, test as baseTest } from '@playwright/test';
 import { ArticlesPage } from '@_src/pages/articles.page';
 import { CommentsPage } from '@_src/pages/comments.page';
 import { HomePage } from '@_src/pages/home.page';
 import { LoginPage } from '@_src/pages/login.page';
+import { RegisterPage } from '@_src/pages/register.page';
+import { test as baseTest } from '@playwright/test';
 
 interface Pages {
   articlesPage: ArticlesPage;
   commentsPage: CommentsPage;
   homePage: HomePage;
-  loginPage: LoginPage
+  loginPage: LoginPage;
+  registerPage: RegisterPage;
 }
 
 export const pageObjectTest = baseTest.extend<Pages>({
-  articlesPage: async({page}, use) => {
+  articlesPage: async ({ page }, use) => {
     const articlesPage = new ArticlesPage(page);
     await articlesPage.goto();
     await use(articlesPage);
   },
-  commentsPage: async({page}, use) => {
+  commentsPage: async ({ page }, use) => {
     const commentsPage = new CommentsPage(page);
     await commentsPage.goto();
     await use(commentsPage);
   },
-  homePage: async({page}, use) => {
+  homePage: async ({ page }, use) => {
     const homePage = new HomePage(page);
     await homePage.goto();
-    await use(homePage)
+    await use(homePage);
   },
-  loginPage: async({page}, use) => {
+  loginPage: async ({ page }, use) => {
     const loginPage = new LoginPage(page);
     await loginPage.goto();
-    await use(loginPage)
-  }
-})
+    await use(loginPage);
+  },
+  registerPage: async ({ page }, use) => {
+    const registerPage = new RegisterPage(page);
+    await registerPage.goto();
+    await use(registerPage);
+  },
+});
