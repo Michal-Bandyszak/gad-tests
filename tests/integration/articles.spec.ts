8195de79c9ff0a13bf6b7befe13a2a203505f768
michal_bandyszak <michalbandyszak@gmail.com>
2024-09-28 18:12:09 +0200

    test: added API check + reformated test

--- tests/integration/articles.spec.ts
+++ tests/integration/articles.spec.ts
@@ -1,24 +1,32 @@
+import { RESPONSE_TIMEOUT } from '@_pw-config';
 import { prepareRandomArticle } from '@_src/factories/article.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
-test.describe('Verify Article', () => {
-  test('reject creating an article without title @GAD-R04-01 @logged', async ({
+test.describe('Verify articles', () => {
+  test('reject creating article without title @GAD-R04-01 @logged', async ({
     addArticleView,
+    page,
   }) => {
     // Arrange
     const expectedErrorMessage = 'Article was not created';
+    const expectedResponseCode = 422;
+
     const articleData = prepareRandomArticle();
     articleData.title = '';
 
+    const responsePromise = page.waitForResponse('/api/articles', {
+      timeout: RESPONSE_TIMEOUT,
+    });
     // Act
     await addArticleView.createArticle(articleData);
+    const response = await responsePromise;
 
     // Assert
-
     await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
+    expect(response.status()).toBe(expectedResponseCode);
   });
 
-  test('reject creating an article without body @GAD-R04-01 @logged', async ({
+  test('reject creating article without body @GAD-R04-01 @logged', async ({
     addArticleView,
   }) => {
     // Arrange
@@ -37,21 +45,23 @@ test.describe('Verify Article', () => {
     test('reject creating article with title exceeding 128 signs @GAD-R04-02 @logged', async ({
       addArticleView,
     }) => {
-      //Arrange
+      // Arrange
       const expectedErrorMessage = 'Article was not created';
       const articleData = prepareRandomArticle(129);
 
       // Act
       await addArticleView.createArticle(articleData);
 
-      //Assert
+      // Assert
       await expect(addArticleView.alertPopup).toHaveText(expectedErrorMessage);
     });
-    test('create article with title length of 128 signs @GAD-R04-02 @logged', async ({
+
+    test('create article with title with 128 signs @GAD-R04-02 @logged', async ({
       addArticleView,
     }) => {
       // Arrange
       const articleData = prepareRandomArticle(128);
+
       // Act
       const articlePage = await addArticleView.createArticle(articleData);
 
