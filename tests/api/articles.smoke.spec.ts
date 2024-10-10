df77ea2a3d383bef045659666bc7e8750fe2d15a
michal_bandyszak <michalbandyszak@gmail.com>
2024-10-10 08:30:45 +0200

    test: return one article

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -12,4 +12,30 @@ test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
 
     expect(response.status()).toBe(expectedStatusCode);
   });
+  test('GET articles should return at least one article @predefined_data', async ({
+    request,
+  }) => {
+    // Arrange
+    const expectedMinArticleCount = 1;
+    const articlesUrl = '/api/articles';
+    // Act
+    const response = await request.get(articlesUrl);
+    const responseJson = await response.json();
+    // Assert
+    expect(responseJson.length).toBeGreaterThanOrEqual(expectedMinArticleCount);
+  });
+  test('GET articles return article object', async ({ request }) => {
+    // Arrange
+    const articlesUrl = '/api/articles';
+    const expectedRequiredFields = ['id', 'body', 'date', 'image', 'user_id'];
+    // Act
+    const response = await request.get(articlesUrl);
+    const responseJson = await response.json();
+    const article = responseJson[0];
+
+    // Assert
+    expectedRequiredFields.forEach((key) => {
+      expect.soft(article).toHaveProperty(key);
+    });
+  });
 });
