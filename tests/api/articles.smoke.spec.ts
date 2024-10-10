b2ca908e6a2a80102b4a44491ee23efbe8373bb0
michal_bandyszak <michalbandyszak@gmail.com>
2024-10-10 08:49:04 +0200

    test: api smoke aggregation test

--- tests/api/articles.smoke.spec.ts
+++ tests/api/articles.smoke.spec.ts
@@ -1,41 +1,35 @@
 import { expect, test } from '@_src/fixtures/merge.fixture';
 
 test.describe('Verify articles API endpoint @GAD-R08-01 @api', () => {
-  test('GET articles returns status code 200', async ({ request }) => {
-    // Arrange
-    const expectedStatusCode = 200;
-    const articlesUrl = '/api/articles';
+  test.describe('Verify each condition in separate test', () => {
+    test('GET articles should return an object with required fields @predefined_data', async ({
+      request,
+    }) => {
+      const articlesUrl = '/api/articles';
+      const response = await request.get(articlesUrl);
+      const responseJson = await response.json();
 
-    // Act
-    const response = await request.get(articlesUrl);
-    // Assert
+      await test.step('GET articles return status code 200', async () => {
+        const expectedStatusCode = 200;
 
-    expect(response.status()).toBe(expectedStatusCode);
-  });
-  test('GET articles should return at least one article @predefined_data', async ({
-    request,
-  }) => {
-    // Arrange
-    const expectedMinArticleCount = 1;
-    const articlesUrl = '/api/articles';
-    // Act
-    const response = await request.get(articlesUrl);
-    const responseJson = await response.json();
-    // Assert
-    expect(responseJson.length).toBeGreaterThanOrEqual(expectedMinArticleCount);
-  });
-  test('GET articles return article object', async ({ request }) => {
-    // Arrange
-    const articlesUrl = '/api/articles';
-    const expectedRequiredFields = ['id', 'body', 'date', 'image', 'user_id'];
-    // Act
-    const response = await request.get(articlesUrl);
-    const responseJson = await response.json();
-    const article = responseJson[0];
+        expect(response.status()).toBe(expectedStatusCode);
+      });
+      await test.step('GET articles should return at least one article @predefined_data', async () => {
+        const expectedMinArticleCount = 1;
 
-    // Assert
-    expectedRequiredFields.forEach((key) => {
-      expect.soft(article).toHaveProperty(key);
+        expect(responseJson.length).toBeGreaterThanOrEqual(
+          expectedMinArticleCount,
+        );
+      });
+
+      const expectedRequiredFields = ['id', 'body', 'date', 'image', 'user_id'];
+      const article = responseJson[0];
+
+      expectedRequiredFields.forEach(async (key) => {
+        await test.step(`Response object contains required field: ${key}`, async () => {
+          expect.soft(article).toHaveProperty(key);
+        });
+      });
     });
   });
 });
