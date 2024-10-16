7c2a86619c9536f70731550b00a6e94f543e3562
michal_bandyszak <michalbandyszak@gmail.com>
2024-10-16 13:10:26 +0200

    test: API tests for comments

---
+++ tests/api/comments.smoke.spec.ts
@@ -0,0 +1,65 @@
+import { expect, test } from '@_src/fixtures/merge.fixture';
+
+test.describe('Verify comments API endpoint @GAD-R08-02 @api', () => {
+  test.describe('verify each condition in separate test', () => {
+    const commentsUrl = '/api/comments';
+    test('GET comments returns status code 200', async ({ request }) => {
+      // Arrange
+      const expectedStatusCode = 200;
+
+      // Act
+      const response = await request.get(commentsUrl);
+
+      //Assert
+      expect(response.status()).toBe(expectedStatusCode);
+    });
+
+    test('GET comments should return at least one comment @predefined_data', async ({
+      request,
+    }) => {
+      // Arrange
+      const expectedCommentsCount = 1;
+      // Act
+      const response = await request.get(commentsUrl);
+      const responseJson = await response.json();
+
+      // Assert
+      expect(responseJson.length).toBeGreaterThanOrEqual(expectedCommentsCount);
+    });
+
+    test('GET comments return comment object @predefined_data', async ({
+      request,
+    }) => {
+      const response = await request.get(commentsUrl);
+      const responseJson = await response.json();
+
+      await expect(
+        typeof responseJson,
+        'Response should return and object',
+      ).toBe('object');
+    });
+
+    test('GET comments should return an object with required fields @predefined_data', async ({
+      request,
+    }) => {
+      const expectedRequiredFields = [
+        'id',
+        'article_id',
+        'user_id',
+        'body',
+        'date',
+      ];
+
+      const response = await request.get(commentsUrl);
+      const responseJson = await response.json();
+
+      const comment = responseJson[0];
+
+      expectedRequiredFields.forEach((key) => {
+        expect
+          .soft(comment, `Expected key "${key} should be in object"`)
+          .toHaveProperty(key);
+      });
+    });
+  });
+});
