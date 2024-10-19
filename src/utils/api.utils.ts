3868428fe50c634dafedeb968a9130ef36d8dc13
michal_bandyszak <michalbandyszak@gmail.com>
2024-10-19 07:56:09 +0200

    chore: refactor added comment util

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,4 +1,5 @@
 import { prepareRandomArticle } from '@_src/factories/article.factory';
+import { prepareRandomComment } from '@_src/factories/comment.factory';
 import { testUser1 } from '@_src/test-data/user.data';
 import { APIRequestContext } from '@playwright/test';
 
@@ -41,4 +42,20 @@ export function prepareArticlePayload(): ArticleData {
   };
 
   return articleData;
+}
+interface CommentData {
+  body: string;
+  date: string;
+  article_id: number;
+}
+
+export function prepareCommentPayload(articleId: number): CommentData {
+  const randomCommentData = prepareRandomComment();
+  const commentData = {
+    body: randomCommentData.body,
+    date: '2024-10-16T15:00:31Z',
+    article_id: articleId,
+  };
+
+  return commentData;
 }
--- tests/api/comments.integration.spec.ts
+++ tests/api/comments.integration.spec.ts
@@ -1,8 +1,8 @@
-import { prepareRandomComment } from '@_src/factories/comment.factory';
 import { expect, test } from '@_src/fixtures/merge.fixture';
 import {
   getAuthorizationBearer,
   prepareArticlePayload,
+  prepareCommentPayload,
 } from '@_src/utils/api.utils';
 
 test.describe('Verify comments CRUD operations @crud @GAD-R09-02', () => {
@@ -29,12 +29,7 @@ test.describe('Verify comments CRUD operations @crud @GAD-R09-02', () => {
     // Arrange
     const expectedStatusCode = 401;
     const commentUrl = '/api/comments';
-    const randomCommentData = prepareRandomComment();
-    const commentData = {
-      body: randomCommentData.body,
-      date: '2024-10-16T15:00:31Z',
-      article_id: 1,
-    };
+    const commentData = prepareCommentPayload(articleId);
 
     // Act
 
@@ -51,16 +46,9 @@ test.describe('Verify comments CRUD operations @crud @GAD-R09-02', () => {
   }) => {
     // Arrange
     const expectedStatusCode = 201;
-
     const commentUrl = '/api/comments';
+    const commentData = prepareCommentPayload(articleId);
 
-    const randomCommentData = prepareRandomComment();
-
-    const commentData = {
-      article_id: articleId,
-      body: randomCommentData.body,
-      date: '2024-10-16T15:00:31Z',
-    };
     headers = await getAuthorizationBearer(request);
     const response = await request.post(commentUrl, {
       headers,
