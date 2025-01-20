5966dcb84bdc57a20d0d1d116d6ccbca1e07f1a1
michal_bandyszak <michalbandyszak@gmail.com>
2025-01-20 10:40:05 +0100

    chore: refactor comment data form paragraphs to sentence

--- src/ui/factories/comment.factory.ts
+++ src/ui/factories/comment.factory.ts
@@ -1,8 +1,8 @@
 import { AddCommentModel } from '@_src/ui/models/comment.model';
 import { faker } from '@faker-js/faker/locale/en';
 
-export function prepareRandomComment(bodySentence = 5): AddCommentModel {
-  const body = faker.lorem.paragraphs(bodySentence);
+export function prepareRandomComment(bodySentence = 3): AddCommentModel {
+  const body = faker.lorem.sentence(bodySentence);
   const newComment: AddCommentModel = { body: body };
 
   return newComment;
